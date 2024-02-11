87b89020e10746565d1aa61629b456db82a106e8
Talat09 <talat181008312@gmail.com>
2024-02-11 19:26:07 +0600

    done middleware protectedRoute

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 //package
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 //file
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToDB from "./db/connectToMongoDB.js";
 //variables
 const app = express();
@@ -10,8 +12,9 @@ const PORT = process.env.PORT || 5000;
 //middleware
 dotenv.config();
 app.use(express.json()); //to parse the incoming request with JSON payloads(from req.body)
+app.use(cookieParser()); //to parse the incoming request with cookies
 app.use("/api/auth", authRoutes);
-
+app.use("/api/messages", messageRoutes);
 app.get("/", (req, res) => {
   res.send("Server is Ready");
 });
