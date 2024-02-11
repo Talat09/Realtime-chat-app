9bcb77e98fecd251dc5bddb973c676cdb5bd027d
Talat09 <talat181008312@gmail.com>
2024-02-11 20:45:54 +0600

    done message controller

--- backend/middleware/protectedRoute.js
+++ backend/middleware/protectedRoute.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/user.model";
+import User from "../models/user.model.js";
 const protectedRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
