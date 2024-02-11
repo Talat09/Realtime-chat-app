9de9f9250d9b8cbf0fe0293dbaf863c455dfe52e
Talat09 <talat181008312@gmail.com>
2024-02-11 13:26:51 +0600

    create controller and connect mongodb

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+  } catch (error) {}
+};
+export const login = (req, res) => {
+  res.send("Login User");
+};
+
+export const logout = (req, res) => {
+  res.send("Logout User");
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import authRoutes from "./routes/auth.routes.js";
 import express from "express";
 import dotenv from "dotenv";
+import connectToDB from "./db/connectToMongoDB.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,4 +11,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server Running on port ${PORT} `));
+app.listen(PORT, () => {
+  connectToDB();
+  console.log(`Server Running on port ${PORT} `);
+});
