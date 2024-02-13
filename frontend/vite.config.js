499fd171988813dc6d7c83a7243912d5e172ed90
Talat09 <talat181008312@gmail.com>
2024-02-14 03:31:13 +0600

    vercel update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        // target: "http://localhost:5000",
-        target: "https://congkoo-app.netlify.app",
+        target: "http://localhost:5000",
       },
     },
   },
