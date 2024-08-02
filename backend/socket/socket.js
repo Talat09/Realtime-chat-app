289151f01f3f5c298d8fb0caac2ed981eaab9637
Talat Mahmud <96833761+Talat09@users.noreply.github.com>
2024-08-02 17:56:21 +0600

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://chat-with-d.vercel.app"],
+    origin: ["https://chat-with-d.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
